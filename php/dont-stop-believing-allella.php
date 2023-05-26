ddbdd4b26d9da93f2daacb4dba55842aa0514f0c
Jim Ciallella <1777776+allella@users.noreply.github.com>
2023-05-26 16:02:34 -0400

    Reorder streetlights and holdin on to a feeling

--- php/dont-stop-believing-allella.php
+++ php/dont-stop-believing-allella.php
@@ -9,8 +9,8 @@
 // while there is not disbelief
 while (! $disbelief)
 {
-	// if streetlights, people, and holding on to a feelin' all exist
-	if ( ($streetlights && $people) && $holdOn == "feelin'" )
+	// if holding on to a feelin' AND streetlights & people exist
+	if ( $holdOn == "feelin'" && ($streetlights && $people) )
 	{
 	  $time = 'midnight';
 	  $destination = 'anywhere';
@@ -24,6 +24,6 @@ while (! $disbelief)
 	  $passenger = 'boy';
 	  $placeOfBirth = 'South Detroit';
 	  $cityBoy = new TrainPassenger($origin, $destination, $passenger, $placeOfBirth, $time);
-	} // end if, "Hold on to that feelin' Streetlights, people"
+	} // end if, "Hold on to that feelin'...Streetlights, people"
 
 } // end while, "Don't Stop Believin'"
