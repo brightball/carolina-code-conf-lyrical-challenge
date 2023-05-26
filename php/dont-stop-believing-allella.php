df60ad620d59e6a5368662dd35272db4213c29e3
Jim Ciallella <1777776+allella@users.noreply.github.com>
2023-05-26 12:41:26 -0400

    PHP - @allella - Tweak class name

--- php/dont-stop-believing-allella.php
+++ php/dont-stop-believing-allella.php
@@ -18,12 +18,12 @@ while (! $disbelief)
 	  $origin = 'small-town';
 	  $passenger = 'girl';
 	  $placeOfBirth = 'lonely world';
-	  $smallTownGirl = new Train ($origin, $destination, $passenger, $placeOfBirth, $time);
+	  $smallTownGirl = new TrainPassenger ($origin, $destination, $passenger, $placeOfBirth, $time);
 
 	  $origin = 'city';
 	  $passenger = 'boy';
 	  $placeOfBirth = 'South Detroit';
-	  $cityBoy = new Train ($origin, $destination, $passenger, $placeOfBirth, $time);
+	  $cityBoy = new TrainPassenger ($origin, $destination, $passenger, $placeOfBirth, $time);
 	} // end if, "Hold on to that feelin' Streetlights, people"
 
 } // end while, "Don't Stop Believin'"
