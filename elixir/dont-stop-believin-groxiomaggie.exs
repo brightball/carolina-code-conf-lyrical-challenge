91aafc5c9c8b906b3dc15b7133e896186837546f
Maggie Tate <49659121+groxiomaggie@users.noreply.github.com>
2023-06-13 22:26:04 -0400

    Create dont-stop-believin-groxiomaggie.exs
    my elixir entry :)

---
+++ elixir/dont-stop-believin-groxiomaggie.exs
@@ -0,0 +1,6 @@
+[she, he] = 
+  [[from: :small_town, gender: :female, pid: lonely_world], 
+  [from: :south_detroit, gender: :male, pid: city]]
+
+send(anywhere, {:midnight_train, she})
+send(anywhere, {:midnight_train, he})
