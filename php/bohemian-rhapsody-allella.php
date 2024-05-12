cc5482bdde109e23203dcce376c0ffff4970962b
Jim Ciallella <1777776+allella@users.noreply.github.com>
2024-05-11 23:21:42 -0400

    PHP, Bohemian Rhapsody, allella, Intro

---
+++ php/bohemian-rhapsody-allella.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* Language: "PHP: Hypertext Preprocessor"
+* Copyright: Queen, 1975
+* Author: @allella
+*/
+
+function intro()
+{
+    try {
+        ($this->life == 'real' ? ($this == 'just fantasy' ? : '') : '';
+    } catch (Landslide $a) {
+       $from = ! esc('reality')
+    }
+}
