561a55ab827f2e9cc1477aa4751959f6c5f512bc
Kevin Curnow <kcurnow@credosoftware.com>
2024-05-20 15:01:08 -0400

    Create BohemianRhapsody.kkernow.sql

---
+++ sql/BohemianRhapsody.kkernow.sql
@@ -0,0 +1,24 @@
+/**
+* @file bohemianrhapsody.kkernow.sql
+* @author Kevin Curnow <kkernow>
+*
+* @section DESCRIPTION
+*
+* Bohemian Rhapsody, in the ~~key~~ language of TSQL.
+
+* Phrase:
+*
+*      Carry on, carry on as if nothing really matters
+*
+*/
+
+DECLARE @Matter varchar(7)
+
+SET @Matter = 'nothing'
+  
+WHILE @Matter is not null
+  BEGIN
+     GO
+     GO
+  END
+
