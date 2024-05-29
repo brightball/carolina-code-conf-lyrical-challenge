9f13d8141693f12620e26bb69b2e7b70ada7da9c
Liz <48806624+alvacat@users.noreply.github.com>
2024-05-29 12:23:40 -0400

    Update bohemian-rhapsody-alvacat.py

--- python/bohemian-rhapsody-alvacat.py
+++ python/bohemian-rhapsody-alvacat.py
@@ -1,6 +1,7 @@
 # If I'm not back this time again tomorrow,
 # Carry on, carry on as if nothing really matters
 
-If check_time == date.today() + timedelta(days=1) and not back:
+start_time = datetime.today()
+If check_time == start_time + timedelta(days=1) and not back:
   what_matters = None
   continue
