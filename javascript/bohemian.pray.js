0e6d6c9893e9408ad03defc756e0b03afd44576f
Prayrana Khadye <103586617+pksuperduper@users.noreply.github.com>
2024-07-07 18:43:56 -0400

    Update bohemian.pray.js

--- javascript/bohemian.pray.js
+++ javascript/bohemian.pray.js
@@ -1,7 +1,7 @@
 // [VERSE 1 -- he is waiting -- "If I'm not back again this time tomorrow"]
 // [VERSE 2 -- time is up -- "Too late, my time has come"]
   
-// This is a way to learn JS Selenium essentials -- explicit (for a condition: isFree) and implicit (specific time: forever) timeouts.
+// This is a way to learn Selenium Javascript essentials -- explicit (for a condition: isFree) and implicit (specific time: forever) timeouts.
 // Waits are important for automation qa -- so you give the server time to respond.  
 
 await driver.wait(until.killer(isFree), forever);
