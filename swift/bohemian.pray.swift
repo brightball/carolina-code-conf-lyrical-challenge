6227388e7f7d59cb3d7cfc9ef9a82c2b4fd3eb09
Prayrana Khadye <103586617+pksuperduper@users.noreply.github.com>
2024-07-14 18:08:02 -0400

    Update bohemian.pray.swift

--- swift/bohemian.pray.swift
+++ swift/bohemian.pray.swift
@@ -2,5 +2,6 @@
 // How it relates to code:  Brute Force approach vs. Efficient Algorithms
 // Poor man's brute force approach.  His mom got 20 million files to see who the killer is.  
 // She has to go through each file manually for him as a poor woman!
+
 let suspects = ["A", "B", "C", "D",...]  
 for suspect in suspects { print("Is this the killer?") }
