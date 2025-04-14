fe15aaa784afcf97da1ad37ab6d6579c9a4d1c1f
Asteri <sarahkmatta@gmail.com>
2025-04-14 16:03:54 -0400

    2025 All Star - csharp - AllStarAsteriHoku.cs

---
+++ csharp/AllStarAsteriHoku.cs
@@ -0,0 +1,73 @@
+// Somebody once told me the world is gonna roll me
+public void Somebody() { Console.WriteLine("The world is gonna roll you"); }
+for (int i = 0; i < 1; i++) { Somebody(); }
+
+// I ain't the sharpest tool in the shed
+!this.IsSharpestTool;
+
+// She was looking kind of dumb with her finger and her thumb
+// In the shape of an "L" on her forehead
+var she = new Human { Finger = true, Thumb = true, Shape = "L", Location = "forehead" };  
+
+// Well, the years start comin' and they don't stop comin'
+while (true) years++;
+
+// Fed to the rules and I hit the ground runnin'
+// Didn't make sense not to live for fun
+if (TheRules(!makesSense() || !liveForFun())) {
+    HitTheGroundRunning();
+}
+
+// Your brain gets smart but your head gets dumb
+brain++; head--;
+
+// So much to do, so much to see
+while (true) thingsToDo++;
+while (true) thingsToSee++;
+
+// So what's wrong with taking the backstreets?
+GetWhatsWrong(dbContext.TravelChoices.Where(tc => tc.Route == "backstreets").Select(tc => tc.Issues));
+
+// You'll never know if you don't go
+this.Go() ? this.Know = true : this.Know = false;
+
+// You'll never shine if you don't glow
+this.Glow() ? this.Shine = true : this.Shine = false;
+
+// Hey now, you're an all star
+var you = new AllStar();
+
+// Get your game on, go play
+GetGameOn(you);
+
+// Hey now, you're a rock star
+you.IsRockStar = true;
+
+// Get the show on, get paid
+StartShow(you);
+GetPaid(you);
+
+// (And all that glitters is gold)
+
+// Only shootin' stars break the mold
+
+// It's a cool place, and they say it gets colder
+bool isCoolPlace = true; bool getsColder = true;
+
+// You're bundled up now, wait 'til you get older
+if (you.IsBundledUp) Task.Delay(TimeSpan.FromDays(UInt64.MaxValue)).Wait();
+
+// But the meteor men beg to differ
+
+// Judging by the hole in the satellite picture
+
+// The ice we skate is gettin' pretty thin
+
+// The water's gettin' warm so you might as well swim
+if (water.isWarm()) { this.Swim(); }
+
+// My world's on fire, how 'bout yours?
+this.World.OnFire = true; you.World.OnFire = IsOnFire();
+
+// That's the way I like it and I'll never get bored
+if (this.World.OnFire) { this.World.LikeIt = true; this.WillGetBored = false; }
