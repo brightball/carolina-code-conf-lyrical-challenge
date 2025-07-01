8de293f5c3b549e54d4ac2b26bfadaed735578e8
Jim Ciallella <1777776+allella@users.noreply.github.com>
2025-07-01 00:57:34 -0400

    2025 Lyrical Code Challenge | PHP | @allella

---
+++ php/allstar-allella.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+* Language: "PHP: Hypertext Preprocessor"
+* Copyright: Smash Mouth, 1999
+* Author: @allella
+*/
+
+function preIntro()
+{
+    $dontStop = ($years->start("comin'") && $they) ? "comin'" : "believin";
+}
